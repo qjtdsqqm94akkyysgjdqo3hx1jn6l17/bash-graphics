b3df62a232b759783dc12c2e84ff1b92c5e8f25b
qjtdsqqm94akkyysgjdqo3hx1jn6l17 <18874@student.vgu.edu.vn>
2025-07-21 21:03:00 +0700

    performance optimaziations in graphic.bash: 	changed some mathematical operations to use binary operations intead. 	no longer initializing the `CANVAS_BUFFER` array upon canvas creation, only use it to store changed pixels from now (and fall back on the `DEFAULT_COLOR` instead). 		- Also no longer atempt to normalized the ([1..n], [1..m]) coordinates into [0..(n-1)*(m-1)] indexing system. things are going as [(m+1)..n*m] instead(should be fine since I'd assume those first indexes just not gonna be created at all probably) 	relying more on Bash's arithmetic expansion for condition checking. 	have the CANVAS_BUFFER 1D array following Fortran (Column-Major) ordering so that we no longer have to add or substract an entire CANVAS_WIDTH between the even and the odd indexes. 	...all of which culminates to we being able inline what was perviously being handled by xy_to_index(), which for some reasons made things 200% faster??

--- graphic.bash
+++ graphic.bash
@@ -12,11 +12,10 @@ error(){
     exit 1
 }
 
-coordinate_valid(){
-    if [ "$x" -gt ${CANVAS_WIDTH:?} ] ||\
-        [ "$y" -gt ${CANVAS_HEIGHT:?} ] ||\
-        [ "$x" -lt 1 ] ||\
-        [ "$y" -lt 1 ]
+coordinate_invalid(){
+    # : ${CANVAS_WIDTH:?} ${CANVAS_HEIGHT:?}
+    # this SEEMED more performant??
+    if ((0<=x<=CANVAS_WIDTH && 1<=y<=CANVAS_HEIGHT))
     then
         return 1
     else
@@ -24,21 +23,24 @@ coordinate_valid(){
     fi
 }
 
+# deprecated
 xy_to_index(){
     local x="${1:?}"
     local y="${2:?}"
-    echo $((CANVAS_WIDTH*(y-1)+(x-1)))
+    # who needs the first 11 indexes in a bash array anyways
+    echo $((x*CANVAS_HEIGHT + y))
 }
 
 # init_canvas <width> <height> [<color_index>]
 init_canvas(){
     # process stuff
-    declare -rg CANVAS_WIDTH="$1" CANVAS_HEIGHT="$2" DRAW_CHAR='▀'
-    color="${3:-0}"
-    local _canvas_size="$( seq 1 "$((CANVAS_WIDTH*CANVAS_HEIGHT))" )"
-    declare -ga CANVAS_BUFFER=( $(printf "$color %.0s" $_canvas_size) )
-    # declare -p CANVAS_BUFFER
-    # sleep 10
+    declare -rg CANVAS_WIDTH="${1:?Must provide a width}" CANVAS_HEIGHT="${2:?Must provide a height}" DRAW_CHAR='▀' DEFAULT_COLOR="${3:-0}"
+
+    # Well just use the Buffer to store any color the canvas is updated with
+    # and fall back to DEFAULT_COLOR if no color was specified in a given index
+    # AKA no need to initialized BUFFER with default values
+    declare -ga CANVAS_BUFFER
+
     get_term_size()
     if [ "$CANVAS_WIDTH" -gt "$COLUMNS" ] \
         || [ "$CANVAS_HEIGHT" -gt "$((LINES*2))" ]
@@ -56,7 +58,7 @@ init_canvas(){
     line_con="$(printf "${DRAW_CHAR}%.0s" $(seq 1 "$CANVAS_WIDTH"))"
     canvas_lines="$(seq 1 "$(((CANVAS_HEIGHT / 2) + (CANVAS_HEIGHT % 2)))")"
     for l in $canvas_lines; do
-        printf '\033[38:5:%bm\033[48:5:%bm%s\033[0m\n' "$color" "$color" "$line_con"
+        printf '\033[38:5:%bm\033[48:5:%bm%s\033[0m\n' "$DEFAULT_COLOR" "$DEFAULT_COLOR" "$line_con"
     done
 }
 
@@ -67,12 +69,14 @@ draw_pixel(){
     local y="${2:?}"
     local color_index="${3:?}"
 
-    if ! coordinate_valid "$x" "$y"
+    if coordinate_invalid "$x" "$y"
     then
         error "Pixel out of bound"
     fi
 
-    local pixel_index="$(xy_to_index "$x" "$y")"
+    # why does doing this inline instead of via xy_to_index() net
+    # a 200% in performance???
+    local pixel_index="$((y+x*CANVAS_HEIGHT))"
     CANVAS_BUFFER[$pixel_index]="$color_index"
 
     # we kinda need to shift y by 1, since
@@ -80,44 +84,36 @@ draw_pixel(){
     # y=2 => line number 2/2 = 1 (yes)
     # y=3 => line number 3/2 = 1 (no! should be 2)
 
-    local line_index="$(((y+1)/2))"
+    # binary operation equiv to (y+1)/2 ()
+    local line_index="$(((y+1)>>1))"
     local col_index="$x" # (just for consistency's sake)
 
-    local pixel_odd_index=0 pixel_even_index=0
+    local pixel_odd_index pixel_even_index
 
-    # printf '\e[%b;0H\033[0J' "$((CANVAS_HEIGHT/2 + 2))"
-    # (exit $((y%2))) is the poor man's `is_even "$y"`
-    if (exit $((y%2))); then
-        pixel_even_index="$pixel_index"
-        pixel_odd_index="$((pixel_index - CANVAS_WIDTH))"
-        # echo "EVEN!! ($y)" >&2
-    else
-        pixel_even_index="$((pixel_index + CANVAS_WIDTH))"
+    # local fg_color bg_color
+
+    # binary operation equiv y%2
+    if ((y & 1)); then
+        pixel_even_index="$((pixel_index + 1))"
         pixel_odd_index="$pixel_index"
-        # echo "ODD!! ($y)" >&2
+        # text "ODD!! ($y)" >&2
+
+    else
+        pixel_even_index="$pixel_index"
+        pixel_odd_index="$((pixel_index - 1))"
+        # text "EVEN!! ($y)" >&2
     fi
 
     # we gotta update 2 pixel at once...
 
-    # declare -p CANVAS_BUFFER
-    # echo "odd=$pixel_odd_index" "even=$pixel_even_index" "index=$pixel_index"
-    # echo "row=$line_index" "col=$col_index"
+
     printf '\033[%b;%bH\033[38:5:%bm\033[48:5:%bm%s\033[0m' \
         "$line_index" "$col_index" \
-        "${CANVAS_BUFFER[$pixel_odd_index]}" \
-        "${CANVAS_BUFFER[$pixel_even_index]}" \
+        "${CANVAS_BUFFER[$pixel_odd_index]-${DEFAULT_COLOR}}" \
+        "${CANVAS_BUFFER[$pixel_even_index]-${DEFAULT_COLOR}}" \
         "${DRAW_CHAR:?}"
 }
 
-# init_canvas_row_col <row> <col> [<color>]
-# init_canvas_row_col(){
-#     # process stuff
-#     declare -rg CANVAS_ROW="$1", CANVAS_COL="$2"
-#     get_term_size()
-#     if [ "$CANVAS_WIDTH" -gt "$COLUMNS" ] || [ "$CANVAS_HEIGHT" -gt "$LINES" ]
-#     # tput smcup
-#     printf '\033[?1049h'
-# }
 
 wait_then_exit_canvas(){
     printf '\033[%b;0H\033[0J' "$((CANVAS_HEIGHT/2 + 2))"
